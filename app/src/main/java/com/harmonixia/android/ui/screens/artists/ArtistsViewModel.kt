604d9d6545d59e66c923d5b97249919c138b0c30
Justin Gasper <jmgasper@gmail.com>
2026-04-14 00:20:55 +1000

    HAR-197: add ExperimentalCoroutinesApi opt-in in ArtistsViewModel
    Add a class-level @OptIn(ExperimentalCoroutinesApi::class) on ArtistsViewModel (with import) to clear the targeted flatMapLatest/transformLatest warning sites without behavior changes.\n\nCo-Authored-By: Paperclip <noreply@paperclip.ing>

--- app/src/main/java/com/harmonixia/android/ui/screens/artists/ArtistsViewModel.kt
+++ app/src/main/java/com/harmonixia/android/ui/screens/artists/ArtistsViewModel.kt
@@ -22,6 +22,7 @@ import com.harmonixia.android.util.PagingStatsTracker
 import com.harmonixia.android.util.mergeWithLocal
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -37,6 +38,7 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.SharingStarted
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class ArtistsViewModel @Inject constructor(
     private val repository: MusicAssistantRepository,
