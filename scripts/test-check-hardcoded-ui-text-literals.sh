284e83095ad186a72ecba53c3c1ee97412f74dc0
jmgasper <jmgasper@gmail.com>
2026-05-07 19:01:34 +1000

    test: cover raw escaped-dollar annotatedstring passes

--- scripts/test-check-hardcoded-ui-text-literals.sh
+++ scripts/test-check-hardcoded-ui-text-literals.sh
@@ -147,6 +147,10 @@ fun Pass(title: String) {
     BasicText(text = AnnotatedString(text = "Now ${title}" /* localized */))
     BasicText(text = AnnotatedString(text = "Now ${title} costs \$5"))
     BasicText(text = AnnotatedString(text = "Now ${title} costs \$5" /* localized */))
+    BasicText(text = AnnotatedString(text = """Now ${title} costs \$5"""))
+    BasicText(text = AnnotatedString(text = """Now ${title} costs \$5""" /* localized */))
+    BasicText(text = AnnotatedString(text = /* localized
+        */ """Now ${title} costs \$5"""))
     BasicText(text = AnnotatedString(text = """$title"""))
     BasicText(text = AnnotatedString(text = /* localized
         */ """$title"""))
