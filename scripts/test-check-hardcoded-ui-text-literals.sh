662a6171c240e1e51a00989a623ac008cfe47a6d
jmgasper <jmgasper@gmail.com>
2026-05-08 07:01:17 +1000

    test: add escaped-dollar raw constructor trailing-line fixtures
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- scripts/test-check-hardcoded-ui-text-literals.sh
+++ scripts/test-check-hardcoded-ui-text-literals.sh
@@ -704,6 +704,25 @@ assert_contains "$escaped_dollar_raw_constructor_comment_fail_output" "EscapedDo
 assert_contains "$escaped_dollar_raw_constructor_comment_fail_output" "AnnotatedString(\"\"\"Price \\\$5\"\"\" /* TODO localize */)"
 assert_contains "$escaped_dollar_raw_constructor_comment_fail_output" "*/ \"\"\"Price \\\$5\"\"\""
 
+escaped_dollar_raw_constructor_line_comment_fail_dir="${tmp_dir}/escaped-dollar-raw-constructor-line-comment-fail"
+mkdir -p "$escaped_dollar_raw_constructor_line_comment_fail_dir"
+cat > "${escaped_dollar_raw_constructor_line_comment_fail_dir}/EscapedDollarRawConstructorLineCommentFail.kt" <<'KOTLIN'
+@Composable
+fun FailEscapedDollarRawConstructorLineComment() {
+    BasicText(text = AnnotatedString("""Price \$5""" // TODO localize
+    ))
+    BasicText(text = AnnotatedString(
+        text = """Price \$5""" // TODO localize
+    ))
+}
+KOTLIN
+
+escaped_dollar_raw_constructor_line_comment_fail_output="$(run_expect_exit 1 "$escaped_dollar_raw_constructor_line_comment_fail_dir")"
+assert_contains "$escaped_dollar_raw_constructor_line_comment_fail_output" "FAIL: hardcoded UI text literals found in Kotlin UI sources:"
+assert_contains "$escaped_dollar_raw_constructor_line_comment_fail_output" "EscapedDollarRawConstructorLineCommentFail.kt"
+assert_contains "$escaped_dollar_raw_constructor_line_comment_fail_output" "AnnotatedString(\"\"\"Price \\\$5\"\"\" // TODO localize"
+assert_contains "$escaped_dollar_raw_constructor_line_comment_fail_output" "text = \"\"\"Price \\\$5\"\"\" // TODO localize"
+
 escaped_dollar_raw_named_arg_close_block_comment_fail_dir="${tmp_dir}/escaped-dollar-raw-named-arg-close-block-comment-fail"
 mkdir -p "$escaped_dollar_raw_named_arg_close_block_comment_fail_dir"
 cat > "${escaped_dollar_raw_named_arg_close_block_comment_fail_dir}/EscapedDollarRawNamedArgCloseBlockCommentFail.kt" <<'KOTLIN'
