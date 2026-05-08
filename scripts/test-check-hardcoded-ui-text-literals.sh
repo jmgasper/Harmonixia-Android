787737ed498bd203faed50273ed93a5800a5de09
jmgasper <jmgasper@gmail.com>
2026-05-09 06:10:58 +1000

    test: cover compact positional raw constructor trailing-inline pass

--- scripts/test-check-hardcoded-ui-text-literals.sh
+++ scripts/test-check-hardcoded-ui-text-literals.sh
@@ -180,6 +180,8 @@ fun Pass(title: String) {
     BasicText(text = AnnotatedString(text = """Now ${title} costs \$5""" // localized
     ))
     BasicText(text = AnnotatedString(text = """Now ${title} costs \$5""" /* localized */))
+    BasicText(text = AnnotatedString("""Now ${title} costs \$5""" /* localized */
+    ))
     BasicText(text = AnnotatedString(
         """Now ${title} costs \$5""" // localized
     ))
