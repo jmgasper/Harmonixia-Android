838210416c0077fe5af518ea46a40517052a1712
jmgasper <jmgasper@gmail.com>
2026-05-09 13:45:20 +1000

    test: cover wrapped named escaped constructor trailing-inline pass

--- scripts/test-check-hardcoded-ui-text-literals.sh
+++ scripts/test-check-hardcoded-ui-text-literals.sh
@@ -153,6 +153,10 @@ fun Pass(title: String) {
         text =
             "Now ${title} costs \$5" // localized
     ))
+    BasicText(text = AnnotatedString(
+        text =
+            "Now ${title} costs \$5" /* localized */
+    ))
     BasicText(text = AnnotatedString(text = /* localized
             */ "Now ${title} costs \$5"))
     BasicText(text = AnnotatedString(text = /* localized
