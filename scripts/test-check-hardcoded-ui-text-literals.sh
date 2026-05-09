c95aa921083298e89ab4d82b5913b19d84b9cf8c
jmgasper <jmgasper@gmail.com>
2026-05-09 18:10:28 +1000

    test: cover wrapped positional escaped constructor trailing-block pass

--- scripts/test-check-hardcoded-ui-text-literals.sh
+++ scripts/test-check-hardcoded-ui-text-literals.sh
@@ -192,6 +192,10 @@ fun Pass(title: String) {
     BasicText(text = AnnotatedString(
         "Now ${title} costs \$5" /* localized */
     ))
+    BasicText(text = AnnotatedString(
+        "Now ${title} costs \$5" /* localized
+            */
+    ))
     BasicText(text = AnnotatedString("""Now ${title} costs \$5"""))
     BasicText(text = AnnotatedString("""Now ${title} costs \$5""" /* localized */))
     BasicText(text = AnnotatedString("""Now ${title} costs \$5""" // localized
