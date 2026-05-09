cc8f4ce28d8da76f6ecbb8cc43e63c599b9f7858
jmgasper <jmgasper@gmail.com>
2026-05-09 22:43:20 +1000

    test: cover icon escaped reordered named close-block pass

--- scripts/test-check-hardcoded-ui-text-literals.sh
+++ scripts/test-check-hardcoded-ui-text-literals.sh
@@ -600,6 +600,11 @@ fun Pass(title: String) {
         imageVector = Icons.Outlined.PlayArrow
     )
     Icon(
+        contentDescription = /* localized
+            */ "Play ${title} for \$5",
+        imageVector = Icons.Outlined.PlayArrow
+    )
+    Icon(
         imageVector = Icons.Outlined.PlayArrow,
         contentDescription = /* localized
             */ """Play ${title} for \$5"""
